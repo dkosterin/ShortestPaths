e3ba60d0740674b31e15f00549af89d59fb975ac
Dmitry Kosterin <kosterin.dim@mail.ru>
2019-05-18 18:24:51 +0300

    add graph class

---
+++ ShortestPaths/ShortestPaths/Graph.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortestPaths
+{
+	class Graph
+	{
+		List<PointF> points = new List<PointF>();
+		List<Barrier> barriers = new List<Barrier>();
+		public Graph(List<PointF> points)
+		{
+			foreach (var p in points)
+				this.points.Add(p);
+		}
+		public void AddBarrier(List<PointF> barrierPoints)
+		{
+			barriers.Add(new Barrier(barrierPoints));
+		}
+		public void Draw(Graphics g)
+		{
+			Pen p = new Pen(Color.Black, 2);
+			for (int i = 0; i < points.Count; i++)
+				g.DrawLine(p, points[i], points[i + 1]);
+			g.DrawLine(p, points[points.Count - 1], points[0]);
+			foreach (var b in barriers)
+				b.Draw(g);
+		}
+	}
+}
